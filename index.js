7ccb469bc542e863b116fd4e2fb4628cce92a2bd
Nikolay Butnik <btnk.nik@gmail.com>
2022-07-11 08:53:26 -0400

    Add a px transform

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const StyleDictionary = require('style-dictionary')
-
-const styleDictionary = StyleDictionary.extend({
+const StyleDictionary = require('style-dictionary').extend({
   source: ['tokens/*.json'],
   platforms: {
     less: {
-      transformGroup: 'less',
+      transformGroup: ['less'],
       buildPath: 'build/less/',
       files: [
         {
@@ -16,6 +14,25 @@ const styleDictionary = StyleDictionary.extend({
   },
 })
 
-// Define custom trnasformations here.
+// First, register a new transform
+StyleDictionary.registerTransform({
+  name: 'size/px',
+  type: 'value',
+  // transitive: true, // use this property to transform referenced values.
+  // If matcher is omitted, all tokens are matched.
+  matcher: (token) => {
+    return token.type === 'dimension' && token.value !== 0
+  },
+  transformer: (token) => {
+    // token.value will be resolved and transformed at this point
+    return `${token.value}px`
+  },
+})
 
-styleDictionary.buildAllPlatforms()
+// Then add the transform to transformGroup
+StyleDictionary.registerTransformGroup({
+  name: 'less', // name of transformGroup in config
+  transforms: StyleDictionary.transformGroup['less'].concat(['size/px']),
+})
+
+StyleDictionary.buildAllPlatforms()
